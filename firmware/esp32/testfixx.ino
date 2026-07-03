2d9063997a0b07c2a7cc5d908f2af97a3eb1f44d
Ficram Manifur Farissa <ficramm@gmail.com>
2026-07-04 02:50:46 +0700

    Update testfixx.ino

--- firmware/esp32/testfixx.ino
+++ firmware/esp32/testfixx.ino
@@ -512,3 +512,22 @@ void loop() {
   delay(1000);
 }
 
+#define BUZZER_PIN 2
+
+void setup() {
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Test Buzzer");
+}
+
+void loop() {
+
+  digitalWrite(BUZZER_PIN, HIGH); // Bunyi
+  delay(1000);
+
+  digitalWrite(BUZZER_PIN, LOW);  // Diam
+  delay(1000);
+
+}
+
